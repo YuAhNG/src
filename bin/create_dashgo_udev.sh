9273f5716c9ad670510b2090fb7dc968da4b5c36
yuhang <YuAhNG@Gmail.com>
2017-07-13 22:39:03 +0800

    serial port config and param changes

--- bin/create_dashgo_udev.sh
+++ bin/create_dashgo_udev.sh
@@ -1,6 +1,8 @@
 echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0042", MODE:="0777", GROUP:="dialout",  SYMLINK+="usb_chassis"' >/etc/udev/rules.d/usb_chassis.rules
-echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", GROUP:="dialout",  SYMLINK+="usb_sensors"' >/etc/udev/rules.d/usb_sensors.rules
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", GROUP:="dialout",  SYMLINK+="usb_mega_sensors"' >/etc/udev/rules.d/usb_mega_sensors.rules
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0777", GROUP:="dialout",  SYMLINK+="usb_imu"' >/etc/udev/rules.d/usb_imu.rules
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0777", GROUP:="dialout",  SYMLINK+="usb_rfid"' >/etc/udev/rules.d/usb_rfid.rules
+echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", MODE:="0777", GROUP:="dialout",  SYMLINK+="usb_uno_lift"' >/etc/udev/rules.d/usb_uno_lift.rules
 service udev reload
 sleep 2
 service udev restart
